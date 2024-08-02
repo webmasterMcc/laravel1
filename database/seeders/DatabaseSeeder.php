4180a4817c121bd36d738161aefa54f0b5d1a780
josepedrolorenzini <webmaster@marbleceramiccorp.com.au>
2024-08-02 15:58:21 +1000

    php artisan migrate:fresh --seed
    php artisan migrate:fresh --seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Job;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,5 +20,26 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+         /// https://laravel.com/docs/11.x/seeding
+        Job::factory()->createMany(
+          [  [
+
+                'title' => 'Director',
+                'employer_id' => 1,
+                'salary' => 50000
+            ],
+            [
+                
+                'title' => 'Programmer',
+                'employer_id' => 2,
+                'salary' => 10000
+            ],
+            [
+                
+                'title' => 'Teacher',
+                'employer_id' => 3,
+                'salary' => 40000
+            ]]
+        );
     }
 }
